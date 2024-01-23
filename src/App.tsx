09553dcd3be7691091e9b6bbf31f21e50272eadf
Hoang Tran <hoang.tran.fin@gmail.com>
2024-01-23 23:48:32 +0200

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,91 +1,24 @@
-import { Euro, LocalShippingOutlined, ShoppingBasketOutlined } from "@mui/icons-material";
-import { Box, Button, Container, Stack, TextField, Typography } from "@mui/material";
-import { useForm, SubmitHandler } from "react-hook-form";
+import { Container, Stack, Typography } from "@mui/material";
+import DeliveryFeeForm from "@src/components/forms/DeliveryFeeForm";
+import { useState } from "react";
 import { calculateDeliveryFee } from "@src/utils";
 
-type Input = {
-	cartValue: number,
-	deliveryDistance: number,
-	numberOfItems: number,
-	orderTime: Date
-};
-
 export default function App() {
-	const {
-		register,
-		handleSubmit,
-		formState: { errors }
-	} = useForm<Input>();
-	const handleSubmission: SubmitHandler<Input> = (data) => console.log(data);
+	const [fee, setFee] = useState<number>(0);
+
+	const handleSubmit = data => console.log(data);
 
 	return (
-		<Container
-			sx={{
-				minHeight: "100%",
-				display: "flex",
-				alignItems: "center",
-				justifyContent: "center"
-			}}
-		>
-			<Stack
-				component="form"
+		<Stack component={Container} minHeight="100%" justifyContent="center" alignItems="center">
+			<DeliveryFeeForm
 				width="clamp(350px, 100%, 500px)"
 				marginX="auto"
-				rowGap={2}
-				noValidate
-				onSubmit={handleSubmit(handleSubmission)}
-			>
-				<Box display="inline-flex" alignItems="flex-end">
-					<Euro sx={{ mr: 1, my: 0.5 }} />
-					<TextField
-						fullWidth
-						label="Cart Value"
-						variant="standard"
-						inputProps={{ "data-test-id": "cartValue" }}
-						{...register("cartValue", { required: true, valueAsNumber: true })}
-					/>
-				</Box>
-
-				<Box display="inline-flex" alignItems="flex-end">
-					<LocalShippingOutlined sx={{ mr: 1, my: 0.5 }} />
-					<TextField
-						fullWidth
-						label="Delivery distance (m)"
-						variant="standard"
-						inputProps={{ "data-test-id": "deliveryDistance" }}
-						{...register("deliveryDistance", { required: true, valueAsNumber: true })}
-					/>
-				</Box>
-
-				<Box display="inline-flex" alignItems="flex-end">
-					<ShoppingBasketOutlined sx={{ mr: 1, my: 0.5 }} />
-					<TextField
-						fullWidth
-						label="Amount of items"
-						variant="standard"
-						inputProps={{ "data-test-id": "numberOfItems" }}
-						{...register("numberOfItems", { required: true, valueAsNumber: true })}
-					/>
-				</Box>
-
-				<TextField
-					type="datetime-local"
-					label="Time"
-					variant="standard"
-					InputLabelProps={{ shrink: true }}
-					inputProps={{ "data-test-id": "orderTime" }}
-					{...register("orderTime", { required: true, valueAsDate: true })}
-				/>
-
-				<Button type="submit" variant="contained">
-					Calculate delivery price
-				</Button>
-				{/* <Typography>{errors}</Typography> */}
-				<Typography>
-					Delivery cost:
-					<Typography component="span" data-test-id="fee">2$</Typography>
-				</Typography>
-			</Stack>
-		</Container>
+				onSubmit={handleSubmit}
+			/>
+			<Typography>
+				Delivery cost:
+				<Typography component="span" data-test-id="fee">{fee}$</Typography>
+			</Typography>
+		</Stack>
 	);
 }
---
+++ src/components/forms/DeliveryFeeForm.tsx
@@ -0,0 +1,63 @@
+import { Box, BoxProps, Button, Stack, TextField } from "@mui/material";
+import { Euro, LocalShippingOutlined, ShoppingBasketOutlined } from "@mui/icons-material";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+	cartValue: number,
+	deliveryDistance: number,
+	numberOfItems: number,
+	orderTime: Date
+};
+
+export default function DeliveryFeeForm(props: BoxProps) {
+	const { register, handleSubmit } = useForm<Inputs>();
+	return (
+		<Box component="form" onSubmit={handleSubmit(props.onSubmit)} rowGap={2} {...props} >
+			<Box display="inline-flex" alignItems="flex-end">
+				<Euro sx={{ mr: 1, my: 0.5 }} />
+				<TextField
+					fullWidth
+					label="Cart Value"
+					variant="standard"
+					inputProps={{ "data-test-id": "cartValue" }}
+					{...register("cartValue")}
+				/>
+			</Box>
+
+			<Box display="inline-flex" alignItems="flex-end">
+				<LocalShippingOutlined sx={{ mr: 1, my: 0.5 }} />
+				<TextField
+					fullWidth
+					label="Delivery distance (m)"
+					variant="standard"
+					inputProps={{ "data-test-id": "deliveryDistance" }}
+					{...register("deliveryDistance")}
+				/>
+			</Box>
+
+			<Box display="inline-flex" alignItems="flex-end">
+				<ShoppingBasketOutlined sx={{ mr: 1, my: 0.5 }} />
+				<TextField
+					fullWidth
+					label="Amount of items"
+					variant="standard"
+					inputProps={{ "data-test-id": "numberOfItems" }}
+					{...register("numberOfItems")}
+				/>
+			</Box>
+
+			<TextField
+				type="datetime-local"
+				label="Time"
+				variant="standard"
+				InputLabelProps={{ shrink: true }}
+				inputProps={{ "data-test-id": "orderTime" }}
+				{...register("orderTime")}
+			/>
+
+			<Button type="submit" variant="contained">
+				Calculate delivery price
+			</Button>
+		</Box>
+	);
+};
