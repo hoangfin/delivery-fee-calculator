10df179dde5d39608eff7d1998c8ac2db3da97f0
Hoang Tran <hoatran@student.hive.fi>
2024-01-23 17:18:24 +0200

    Updated

--- src/components/DeliveryFeeForm.tsx
+++ src/components/DeliveryFeeForm.tsx
@@ -1,39 +1,14 @@
+import { Box, Button, Stack, TextField } from "@mui/material";
 import { Euro, LocalShippingOutlined, ShoppingBasketOutlined } from "@mui/icons-material";
-import { Box, Button, Container, Stack, TextField, Typography } from "@mui/material";
-import { useForm, SubmitHandler } from "react-hook-form";
-import { calculateDeliveryFee } from "@src/utils";
 
-type Input = {
-	cartValue: number,
-	deliveryDistance: number,
-	numberOfItems: number,
-	orderTime: Date
-};
-
-export default function App() {
-	const {
-		register,
-		handleSubmit,
-		formState: { errors }
-	} = useForm<Input>();
-	const handleSubmission: SubmitHandler<Input> = (data) => console.log(data);
-
+export default function DeliveryFeeForm() {
 	return (
-		<Container
-			sx={{
-				minHeight: "100%",
-				display: "flex",
-				alignItems: "center",
-				justifyContent: "center"
-			}}
-		>
+		<form>
 			<Stack
 				component="form"
 				width="clamp(350px, 100%, 500px)"
 				marginX="auto"
 				rowGap={2}
-				noValidate
-				onSubmit={handleSubmit(handleSubmission)}
 			>
 				<Box display="inline-flex" alignItems="flex-end">
 					<Euro sx={{ mr: 1, my: 0.5 }} />
@@ -42,7 +17,6 @@ export default function App() {
 						label="Cart Value"
 						variant="standard"
 						inputProps={{ "data-test-id": "cartValue" }}
-						{...register("cartValue", { required: true, valueAsNumber: true })}
 					/>
 				</Box>
 
@@ -53,7 +27,6 @@ export default function App() {
 						label="Delivery distance (m)"
 						variant="standard"
 						inputProps={{ "data-test-id": "deliveryDistance" }}
-						{...register("deliveryDistance", { required: true, valueAsNumber: true })}
 					/>
 				</Box>
 
@@ -64,7 +37,6 @@ export default function App() {
 						label="Amount of items"
 						variant="standard"
 						inputProps={{ "data-test-id": "numberOfItems" }}
-						{...register("numberOfItems", { required: true, valueAsNumber: true })}
 					/>
 				</Box>
 
@@ -74,18 +46,12 @@ export default function App() {
 					variant="standard"
 					InputLabelProps={{ shrink: true }}
 					inputProps={{ "data-test-id": "orderTime" }}
-					{...register("orderTime", { required: true, valueAsDate: true })}
 				/>
 
 				<Button type="submit" variant="contained">
 					Calculate delivery price
 				</Button>
-				{/* <Typography>{errors}</Typography> */}
-				<Typography>
-					Delivery cost:
-					<Typography component="span" data-test-id="fee">2$</Typography>
-				</Typography>
 			</Stack>
-		</Container>
+		</form>
 	);
-}
+};
