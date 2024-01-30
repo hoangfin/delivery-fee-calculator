5a635b0df9c7db6d3efe90748bf62f6f0723f107
Hoang Tran <hoang.tran.fin@gmail.com>
2024-01-30 23:45:05 +0200

    Added new tests

--- src/utils/calculateDeliveryFee.test.ts
+++ src/utils/calculateDeliveryFee.test.ts
@@ -1,33 +1,37 @@
 import { calculateDeliveryFee } from "./calculateDeliveryFee";
-import type { FormData } from "@src/components/forms/DeliveryFeeForm";
 
-const data: FormData = {
-	cartValue: 10,
-	deliveryDistance: 1000,
-	numberOfItems: 4,
-	orderTime: new Date(2024, 0, 22, 15, 30, 0, 0)
-};
-
-test("calculate delivery fee when cartValue = 10, deliveryDistance = 1000, numberOfItems = 4, orderTime = Friday Jan 22 2024 15:30:00", () => {
-	expect(calculateDeliveryFee(data)).toBe(2);
+test("cartValue = 10, deliveryDistance = 1000, numberOfItems = 4, orderTime = Mon Jan 22 2024 15:30:00", () => {
+	expect(calculateDeliveryFee({
+		cartValue: 10,
+		deliveryDistance: 1000,
+		numberOfItems: 4,
+		orderTime: new Date(2024, 0, 22, 15, 30, 0, 0)
+	})).toBe(2);
 });
 
-// test("calculate delivery fee when cart value = 8, numberOfItems = 4", () => {
-// 	expect(calculateDeliveryFee(data)).toBe(2);
-// });
+test("cartValue = 200, deliveryDistance = 1000, numberOfItems = 4, orderTime = Mon Jan 22 2024 15:30:00", () => {
+	expect(calculateDeliveryFee({
+		cartValue: 200,
+		deliveryDistance: 1000,
+		numberOfItems: 4,
+		orderTime: new Date(2024, 0, 22, 15, 30, 0, 0)
+	})).toBe(0);
+});
 
-// test("calculate delivery fee when cart value = 8, numberOfItems = 4", () => {
-// 	expect(calculateDeliveryFee(data)).toBe(2);
-// });
+test("cartValue = 50, deliveryDistance = 1501, numberOfItems = 4, orderTime = Mon Fri 26 2024 15:30:00", () => {
+	expect(calculateDeliveryFee({
+		cartValue: 50,
+		deliveryDistance: 1501,
+		numberOfItems: 4,
+		orderTime: new Date(2024, 0, 26, 15, 30, 0, 0)
+	})).toBe(4.8);
+});
 
-// test("calculate delivery fee when cart value = 8, numberOfItems = 4", () => {
-// 	expect(calculateDeliveryFee(data)).toBe(2);
-// });
-
-// test("calculate delivery fee when cart value = 8, numberOfItems = 4", () => {
-// 	expect(calculateDeliveryFee(data)).toBe(2);
-// });
-
-// test("calculate delivery fee when cart value = 8, numberOfItems = 4", () => {
-// 	expect(calculateDeliveryFee(data)).toBe(2);
-// });
+test("cartValue = 50, deliveryDistance = 4001, numberOfItems = 13, orderTime = Mon Thu 25 2024 15:30:00", () => {
+	expect(calculateDeliveryFee({
+		cartValue: 50,
+		deliveryDistance: 4001,
+		numberOfItems: 13,
+		orderTime: new Date(2024, 0, 25, 15, 30, 0, 0)
+	})).toBe(14.7);
+});
