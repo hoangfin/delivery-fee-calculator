bf1b47840309572a5e588b745ab45dbc03a5002d
Hoang Tran <hoatran@student.hive.fi>
2024-01-30 16:15:05 +0200

    Updated schema and README file

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# delivery-fee-calculator
+# Delivery Fee Calculator - Wolt Summer 2024 Engineering Internships
+
+This code is needed when a customer is ready with their shopping cart and we’d
+like to show them how much the delivery will cost. The delivery price depends on
+the cart value, the number of items in the cart, the time of the order, and the
+delivery distance.
+
+### Installation & Run
+<ol>
+	<li>Unzip `calculator.zip`</li>
+	<li>Go in the unzipped directory `calculator`</li>
+	<li>Open up terminal and run command `npm install`</li>
+	<li>Once finished, run command `npm run dev`</li>
+	<li>Open web browser, enter the following address `http://localhost:5173/`</li>
+</ol>
--- src/components/forms/DeliveryFeeForm/schema.ts
+++ src/components/forms/DeliveryFeeForm/schema.ts
@@ -7,15 +7,19 @@ export const schema: ZodType<FormData> = z.object({
 		z.number({ invalid_type_error: "Number expected" })
 		 .gt(0, "Cart value must be greater than 0")
 	) as z.ZodEffects<z.ZodNumber, number, number>,
+
 	deliveryDistance: z.preprocess(
 		value => value === "" ? undefined : Number(value),
 		z.number({ invalid_type_error: "Number expected" })
+		 .gt(0, "Delivery distance must be greater than 0")
 	) as z.ZodEffects<z.ZodNumber, number, number>,
+
 	numberOfItems: z.preprocess(
 		value => value === "" ? undefined : Number(value),
 		z.number({ invalid_type_error: "Number expected" })
 		 .int("Integer is required")
 		 .gt(0, "Number of items must be greater than 0")
 	) as z.ZodEffects<z.ZodNumber, number, number>,
+
 	orderTime: z.coerce.date()
 });
